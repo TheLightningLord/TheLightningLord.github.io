d23f7b8b02191f9f21fb1460901afdf9af3abc46
Zeuse Valentine <valentze@oregonstate.edu>
2025-03-03 06:25:25 -0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,1 +1,18 @@
+export default function Footer() {
+  return (
+    <footer style={{
+      padding: '1rem',
+      backgroundColor: '#222',
+      color: '#fff',
+      textAlign: 'center'
+    }}>
+      <p>&copy; {new Date().getFullYear()} Olympian Health Solutions. All rights reserved.</p>
+      <p>
+        Follow us: 
+        <a href="https://facebook.com" style={{ margin: '0 0.5rem', color: '#fff' }}>Facebook</a> | 
+        <a href="https://twitter.com" style={{ margin: '0 0.5rem', color: '#fff' }}>Twitter</a>
+      </p>
+    </footer>
+  );
+}
 
