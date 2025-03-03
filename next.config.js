96b1724f41b90775b6a75f140e83fddb8a1deedb
Zeuse Valentine <valentze@oregonstate.edu>
2025-03-03 06:43:11 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,11 @@
+ /** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export', // Enables static export
+  images: {
+    unoptimized: true, // Prevents image optimization (since Vercel/AWS optimize it automatically)
+  },
+};
+
+module.exports = nextConfig;
 
